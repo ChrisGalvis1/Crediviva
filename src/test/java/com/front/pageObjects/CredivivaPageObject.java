28fcb3448d920769ebe3b4f56a7e8411b9a68b29
ChrisGalvis1 <galvischris@gmail.com>
2024-03-16 13:01:21 -0500

    Ultimos pasos terminados

--- src/test/java/com/front/pageObjects/CredivivaPageObject.java
+++ src/test/java/com/front/pageObjects/CredivivaPageObject.java
@@ -1,6 +1,7 @@
 package com.front.pageObjects;
 
 import com.front.runners.RunnerFlujosCrediviva;
+import com.front.utilidades.Utilidades;
 import net.serenitybdd.core.Serenity;
 import net.serenitybdd.core.pages.PageObject;
 import org.junit.Assert;
@@ -8,13 +9,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.openqa.selenium.WebDriver;
 
 public class CredivivaPageObject extends PageObject {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RunnerFlujosCrediviva.class);
+    Utilidades utilidades;
     //Objetos mapeados
-    String txtBusquedaGoogle = "//form[@action='/search']//div[@class='RNNXgb']//div[@class='a4bIc']";
+    String txtBusquedaGoogle = "//form[@action='/search']//div[@class='RNNXgb']//div[@class='a4bIc']//textarea";
     String btnSeleccionarBusqueda = "//div[@role='main']//div[@id='search']//div[@class='BYM4Nd']//div[@class='eKjLze']//div[@aria-hidden='true']";
+    String btnBlogCrediviva = "//div[@id='page']//div[@class='elementor elementor-1499 elementor-location-header']//div[@class='elementor-container elementor-column-gap-no']//div[@data-id='76fb61c2']//div[@data-element_type='widget']//div[@id='ekit-megamenu-menu_2023']//ul[@id='menu-menu_2023']//li[@id='menu-item-2009']//a[text()='Blog']";
+    String lblBlogCrediviva = "//div[@id='content']//div[@id='primary']//section[@data-id='54856cde']//div[@class='elementor-widget-wrap elementor-element-populated']//div[@data-id='84465bf']//div[@class='elementor-widget-container']";
+
 
     public void digitarEnElBuscadorLaPalabraCrediviva(String textoBuscar) {
         try {
@@ -31,7 +37,6 @@ public class CredivivaPageObject extends PageObject {
         try {
             find(By.xpath(btnSeleccionarBusqueda)).click();
             waitFor(2).seconds();
-            Serenity.takeScreenshot();
         }catch (Exception e){
             LOGGER.error("Falló en el paso seleccionarLaOpcionBuscada: " + e.getMessage());
             Assert.assertTrue(false);
@@ -40,7 +45,8 @@ public class CredivivaPageObject extends PageObject {
 
     public void irOpcionDeBlogDeLaPagina() {
         try {
-
+            utilidades.bordearElemento(find(By.xpath(btnBlogCrediviva)));
+            find(By.xpath(btnBlogCrediviva)).click();
         }catch (Exception e){
             LOGGER.error("Falló en el paso irOpcionDeBlogDeLaPagina: " + e.getMessage());
             Assert.assertTrue(false);
@@ -49,7 +55,10 @@ public class CredivivaPageObject extends PageObject {
 
     public void validarQueLaPalabraBlogCredivivaEsteVisibileEnLaPaginaTomarUnaCapturaDePantalla() {
         try {
-
+            utilidades.bordearElemento(find(By.xpath(lblBlogCrediviva)));
+            Assert.assertTrue(find(By.xpath(lblBlogCrediviva)).isDisplayed());
+            waitFor(1).second();
+            utilidades.takeScreenShot(getDriver(), "screeShot_BlogCrediviva.png");
         }catch (Exception e){
             LOGGER.error("Falló en el paso validarQueLaPalabraBlogCredivivaEsteVisibileEnLaPaginaTomarUnaCapturaDePantalla: " + e.getMessage());
             Assert.assertTrue(false);
--- src/test/java/com/front/utilidades/Utilidades.java
+++ src/test/java/com/front/utilidades/Utilidades.java
@@ -1,9 +1,15 @@
 package com.front.utilidades;
 
+import net.serenitybdd.core.Serenity;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
+
+import java.io.File;
 
 public class Utilidades extends PageObject {
 
@@ -14,5 +20,11 @@ public class Utilidades extends PageObject {
     public void posicionarElemento(WebElementFacade strWebElement) {
         Actions actions=new Actions(getDriver());
         actions.moveToElement(strWebElement).perform();
+    }
+
+    public void takeScreenShot (WebDriver driver, String screenShotName){
+        File screenShotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        Serenity.recordReportData().withTitle("ScreenShots").andContents(screenShotName);
+        screenShotFile.renameTo(new File("target/site/screenShots_passed", screenShotName));
     }
 }
